c623b2261dece61675d37c354eeecc921acc3e0f
Elvis <okaforelvis24@gmail.com>
2020-06-22 07:35:59 -0700

    initial

---
+++ omo.js
@@ -0,0 +1,38 @@
+function Player(myName, myScore) {
+    this.name = myName;
+    this.score = myScore;
+}
+
+
+function displayLeaderboard(Players) {
+    let theExport = "";
+    Players.sort((aPlayer, bPlayer) => bPlayer.score - aPlayer.score);
+    Players.forEach((player) => theExport += '<tr><td>' + player.name + '</td><td>' + player.score + '</td></tr>');
+    document.getElementById("thingy").innerHTML = theExport; //Why have good ID's when you can have bad ones? | Who needs children when we can use innerHTML?
+}
+
+let players;
+
+function render() {
+    fetch("./players.json")
+        .then(resp => resp.json())
+        .then(players => {
+            displayLeaderboard(players);
+        })
+        .catch(err => {
+
+        })
+
+}
+
+function displayLeaderboard(data) {
+    let theExport = "";
+    //clear old dom
+    const Players = [...data]
+    Players.sort((aPlayer, bPlayer) => bPlayer.score - aPlayer.score);
+    Players.forEach((player) => theExport += '<tr><td>' + player.name + '</td><td>' + player.score + '</td></tr>');
+    document.getElementById("thingy").innerHTML = theExport; //Why have good ID's when you can have bad ones? | Who needs children when we can use innerHTML?
+}
+
+
+render();
